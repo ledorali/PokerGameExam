58198189d1c93b0d4a635057f23fb4ca7bb40681
Rodel Bolima <rodel.bolima@gmail.com>
2019-09-27 13:49:09 +0800

    Updated Packages for Unittest
    Updated Packages for Unittest

---
+++ PokerGameTest/GameExceptionTest.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyLibrary;
+
+namespace PokerGameTest
+{
+    [TestClass]
+    public class GameExceptionTest
+    {
+        [TestMethod()]
+        public void TestIsDuplicateCard()
+        {
+            //Arrange
+            PlayPokerGame PokerFirstGame = new PlayPokerGame();
+            PokerPlayer Rodel = new PokerPlayer("Rodel");
+            PokerFirstGame.AddPlayer(Rodel);
+
+            PokerFirstGame.GivePlayingCards(Rodel, new PlayingCard { Rank = RankType.Eight, Suit = SuitCard.Clubs });
+            PokerFirstGame.GivePlayingCards(Rodel, new PlayingCard { Rank = RankType.Six, Suit = SuitCard.Heart });
+            PokerFirstGame.GivePlayingCards(Rodel, new PlayingCard { Rank = RankType.Eight, Suit = SuitCard.Heart });
+            PokerFirstGame.GivePlayingCards(Rodel, new PlayingCard { Rank = RankType.Jack, Suit = SuitCard.Heart });
+            PokerFirstGame.GivePlayingCards(Rodel, new PlayingCard { Rank = RankType.Eight, Suit = SuitCard.Diamond });
+            PokerFirstGame.GivePlayingCards(Rodel, new PlayingCard { Rank = RankType.Eight, Suit = SuitCard.Diamond });
+
+            //Act
+            try
+            {
+                PokerHand pokerHand = PokerFirstGame.GetWinner();
+            }
+            catch (Exception ex)
+            {
+                //Assert            
+                StringAssert.Contains(ex.Message.ToString(), "Duplicate card(s) found");
+                return;
+            }
+
+            Assert.Fail("No exception was thrown.");
+
+        }
+        [TestMethod]
+        public void TestIsDuplicatePlayer()
+        {
+            //Arrange
+            PlayPokerGame PokerFirstGame = new PlayPokerGame();
+
+            PokerPlayer Batman = new PokerPlayer("Batman");
+            PokerPlayer Robin = new PokerPlayer("Batman");
+
+            //Act
+            try
+            {
+                PokerFirstGame.AddPlayer(Batman);
+                PokerFirstGame.AddPlayer(Robin);
+            }
+            catch (Exception ex)
+            {
+                //Assert            
+                StringAssert.Contains(ex.Message.ToString(), "Player name already exists");
+                return;
+            }
+
+            Assert.Fail("No exception was thrown.");
+        }
+
+
+    }
+}
+
